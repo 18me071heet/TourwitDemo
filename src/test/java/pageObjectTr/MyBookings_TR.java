60651d06a081440348f582e229d2fe1503385eda
Heet <18ME071.heet.nanda@vvpedulink.ac.in>
2025-09-08 17:21:00 +0530

    Profile,My Order,Search and View Order,Notification

---
+++ src/test/java/pageObjectTr/MyBookings_TR.java
@@ -0,0 +1,108 @@
+package pageObjectTr;
+
+import java.time.Duration;
+import org.openqa.selenium.TimeoutException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyBookings_TR {
+
+	public WebDriver driver;
+	
+	public MyBookings_TR(WebDriver driver) {
+		
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//input[@id='search']")
+	WebElement search;
+    
+	@FindBy(xpath="//img[@alt='Logo']")
+	WebElement homeLogo;
+
+	public void myBookings() {
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		WebElement btnBookings= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[normalize-space()='My Bookings']")));
+		btnBookings.click();
+		
+	}
+	
+	public void serachId(String id) {
+		
+		search.sendKeys(id);
+	}
+	
+	public void viewDetails() {
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		WebElement viewDetailsLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//a[contains(text(),'View Details')]")));
+		viewDetailsLink.click();
+		
+	}
+	
+	public void clickBack() {
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		WebElement clickBackArrow = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[local-name()='svg' and @class='h-6 w-6']")));
+		clickBackArrow.click();
+	}
+	
+	public void homeLogoClick() {
+		
+		homeLogo.click();
+	}
+	
+	public void clickNotificationIcon() {
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		WebElement notiIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/notification']")));
+		notiIcon.click();
+			
+	}
+	
+	public void clearAllNoti() {
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		WebElement notiClear = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(" //button[normalize-space()='Clear All']")));
+		notiClear.click();
+	}
+	
+	public void moreNotification() throws InterruptedException {
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		
+		try {
+			
+			WebElement showMore = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(" //button[normalize-space()='Load More']")));
+			
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].scrollBy(0,200);", showMore);
+			Thread.sleep(2000);
+			
+			if(showMore.isDisplayed()) {
+				showMore.click();	
+			} else {
+				
+				System.out.println("Show more button is not found");
+			}
+			
+			
+		}catch(TimeoutException e) {
+			
+			System.out.println("Show more button is not displaying");
+		}
+		
+	}
+	
+	
+	
+}
---
+++ src/test/java/tc_Tr/MyBookings_Tr.java
@@ -0,0 +1,111 @@
+package tc_Tr;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import baseClassTr.BaseClassTr;
+import pageObjectTr.Login_TR;
+import pageObjectTr.MyBookings_TR;
+
+public class MyBookings_Tr extends BaseClassTr {
+
+	 public void threadTime() throws InterruptedException{
+		 
+		 Thread.sleep(4000);
+	 }
+	 
+	 @Test(priority=1)
+	   void logInDetails() throws InterruptedException {
+		
+		Login_TR login = new Login_TR(driver);
+		
+		try {
+			
+			logger.info("TC-01 --> Verify User is able to add email");
+          login.addEmail(p.getProperty("email_tr"));
+	   		
+	   		logger.info("TC-02 --> Verify User is able to add password");
+	   		login.addPass(p.getProperty("password_tr"));
+	   		
+	   		logger.info("TC-03 --> Verify User is able to login by clicking on Login Button");
+          login.loginBtn();
+	   		
+		}  catch(Exception e) {
+			
+			logger.error("Failed:"+e);
+			Assert.fail("Failed due to:"+e.getMessage());
+		}
+		
+		
+	}
+	 
+	 @Test(priority=2,dependsOnMethods= {"logInDetails"})
+	 void userBookings() throws InterruptedException {
+		 
+		 Login_TR login = new Login_TR(driver);
+		 
+		 logger.info("TC-01 --> Verify options are displaying by clicking on profile icon");
+		 threadTime();
+		 login.profileIconClick();
+		 
+		 MyBookings_TR bookings = new MyBookings_TR(driver);
+		 
+		 logger.info("TC-02 --> Verify User is navigate to the booking screen by clicking on My Bookings");
+		 
+		 bookings.myBookings();
+		 
+		 logger.info("TC-03 --> Verify User is able to search their booking by their order id");
+		 
+		 bookings.serachId(p.getProperty("orderIdSearch_Tr"));
+		 
+		 logger.info("TC-04 --> Verify User is able to see their searched order details by clicking on view details ");
+		 
+		 bookings.viewDetails();
+		 
+		 logger.info("TC-05 --> Verify User is navigate to the booking listing page by clicking on Back arrow");
+		 
+		 bookings.clickBack();
+		 
+		 logger.info("TC-06 --> Verify User is able to search another order");
+		 
+		 threadTime();
+		 bookings.serachId(p.getProperty("secondOrderId_Tr"));
+		 
+		 logger.info("TC-07 --> Verify User is able to see another order details by clicking on view details");
+		 threadTime();
+		 bookings.viewDetails();
+		 
+		 bookings.clickBack();
+		 
+		 logger.info("TC-08 --> Verify User is able to filter the bookings by Newest to Oldest / Oldest to Newest");
+		 
+		 WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		 WebElement filter = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'control')]")));
+		 filter.click();
+		 
+		 WebElement option = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'option') and text()='Newest to Oldest']")));
+		 option.click();
+		 threadTime();
+		 
+		 logger.info("TC-09 --> Verify User is navigate to the home page by clicking on Home logo");
+		 bookings.homeLogoClick();
+		 		 
+		 logger.info("TC-10 --> Verify User is navigate to the Notification page by clicking on Notification icon");
+		 threadTime();
+		 bookings.clickNotificationIcon();
+		 
+		 logger.info("TC-11 --> Verify User is able to see more notifications by clicking on show more");
+		 bookings.moreNotification();
+		 
+		 logger.info("TC-12 --> Verify Notification is getting cleared by clicking Clear All");
+		 bookings.clearAllNoti();
+		 
+			 
+	 }
+	 
+}
