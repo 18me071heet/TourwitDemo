ea3e119ed9bcf88a4fb8eaa267ebb93782fe5de1
Heet <18ME071.heet.nanda@vvpedulink.ac.in>
2025-11-05 17:45:07 +0530

    Edit profile Details ,Logs , Reports

--- src/test/java/tc_Tr/UserProfile_Tr.java
+++ src/test/java/tc_Tr/UserProfile_Tr.java
@@ -30,7 +30,7 @@ public class UserProfile_Tr extends BaseClassTr{
  		try {
  			
  			logger.info("TC-01 --> Verify User is able to add email");
-            login.addEmail(p.getProperty("deleteAccountEmail_tr"));
+            login.addEmail(p.getProperty("email_tr"));
 	   		
 	   		logger.info("TC-02 --> Verify User is able to add password");
 	   		login.addPass(p.getProperty("password_tr"));
@@ -46,7 +46,7 @@ public class UserProfile_Tr extends BaseClassTr{
  		
  	}
 	 
-	// @Test(priority=2,dependsOnMethods= {"logInDetails"})
+	 @Test(priority=2,dependsOnMethods= {"logInDetails"})
 	 void editProfile() throws InterruptedException {
 		 
 		 try {
@@ -63,36 +63,45 @@ public class UserProfile_Tr extends BaseClassTr{
 			 
 			 logger.info("TC-02 --> Verify User is navigate to the profile page by clicking on My profile");
 			 profile.myProfileClick();
+
 			 
 			 threadTime();
-			 logger.info("TC-03 --> Verify User is able to Edit their first name");
+			       	 
+	       	 logger.info("Tc -03 Verify User is able to enter name and email address in subscription popup ");
+	       	 
+	       	 logger.info("TC- 04 --> Verify User is able to select terms and conditions checkbox");
+	       	 
+	       	 logger.info("TC-05--> Verify User is able to subscribe news letter by clicking submit button");
+	       	 login.subscribeNewsLater("Will John", "user23@yopmail.com");
+						
+			 logger.info("TC-06 --> Verify User is able to Edit their first name");
 			 
 			 profile.editFirstName();
 			 
-			 profile.editFnames("Will");
+			 profile.editFnames(p.getProperty("editFname"));
 			 
-			 logger.info("TC-04 --> Verify Update name is getting saved by clicking on Save");
+			 logger.info("TC-07 --> Verify Update name is getting saved by clicking on Save");
 			 
 			 profile.btnSaveClick();
 			 threadTime();
 			 
-			 logger.info("TC-05 --> Verify User is able to Edit their last name");
+			 logger.info("TC-08 --> Verify User is able to Edit their last name");
 			 
 			 profile.editLastName();
 			 
-			 profile.editLnames("John");
+			 profile.editLnames(p.getProperty("editLname"));
 			 
 			 profile.btnSaveClick();
 			 
-			 logger.info("TC-06 --> Verify User is able to Edit their Insta Id");
+			 logger.info("TC-09 --> Verify User is able to Edit their Insta Id");
 			 
 			 profile.editInsta();
 			 
-			 profile.editInstaId("Will100");
+			 profile.editInstaId(p.getProperty("editInstaid"));
 			 
 			 profile.btnSaveClick();
 			 
-			 logger.info("TC-07 --> Verify User is able to Edit their Country from Country dropdown");
+			 logger.info("TC-10 --> Verify User is able to Edit their Country from Country dropdown");
 			 
 			 threadTime();
 			 profile.editCountry();
@@ -102,33 +111,34 @@ public class UserProfile_Tr extends BaseClassTr{
 			 country.click();
 			 country.sendKeys("Australia");
 			 
-			 logger.info("TC-08 --> Verify Selected country is displaying in the country field");
+			 logger.info("TC-11 --> Verify Selected country is displaying in the country field");
 			 
 			 WebElement countryOptionSelected = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@id,'react-select-lazy-country-option') and text()='Australia']")));
 			 countryOptionSelected.click();
 			
 			 profile.btnSaveClick();
 			 
-			 logger.info("TC-09 --> Verify User is able to view their followers list");
+			 logger.info("TC-12 --> Verify User is able to view their followers list");
 			 
 			 Thread.sleep(3000);
 			 
 			 // profile.clickFollowersView();
 			 
-			 logger.info("TC-10 --> Verify User is able to search specific follower");
+			 logger.info("TC-13 --> Verify User is able to search specific follower");
 		
 		     //	 profile.search("Lisa Menon");
 			 
-			 logger.info("TC-11 --> Verify User is able to view their following list");
+			 logger.info("TC-14 --> Verify User is able to view their following list");
 			 
 			 profile.clickFollowingView();
 			 
-			 logger.info("TC-12 --> Verify User is able to search specific following");
+			 logger.info("TC-15 --> Verify User is able to search specific following");
 			 
 			 profile.search(p.getProperty("followingSearch_Tr"));
 			 
-			 logger.info("TC-13 --> Verify User is able to unfollow user by clicking on Unfollow");
+			 logger.info("TC-16 --> Verify User is able to unfollow user by clicking on Unfollow");
 			 
+			 Thread.sleep(4000);
 			 profile.unfollowUser(); 
 			 
 		 } catch(Exception e) {
@@ -139,7 +149,7 @@ public class UserProfile_Tr extends BaseClassTr{
 				 		 
 	 }
 	 
-	 @Test(priority=3,dependsOnMethods= {"logInDetails"})
+	// @Test(priority=3,dependsOnMethods= {"logInDetails"})
 	 void deleteMyAccount() throws InterruptedException {
 		 
 		 try {
